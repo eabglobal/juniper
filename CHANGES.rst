61137923570d8108ab3c89b976c3eb9b7eccd2fa
Pedro Diaz <pedro.diaz.vargas@gmail.com>
2019-02-01 18:34:15 -0500

    WIP

--- CHANGES.rst
+++ CHANGES.rst
@@ -3,6 +3,14 @@
 Juniper Changelog
 ===============
 
+Version 0.2
+-----------
+
+Released on February 1st 2019, codename Fondo 2
+
+- Fixing issues with the juniper package deployment.
+
+
 Version 0.1
 -----------
 
--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
+import io
 from setuptools import setup, find_packages
 
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
 
+# Make sure that the version of the package always matches the version of the
+# tool. The version of the __init__.py is updated with the make release command.
+with io.open('juniper/__init__.py', 'rt', encoding='utf8') as f:
+    version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 
 setup(
     name="juniper",
-    version='0.1.0',
+    version=version,
     author='EAB Tech',
     author_email='eabtech@eab.com',
     description="Tool to streamline the build of python lambda functions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    project_urls=OrderedDict((
+        ('Documentation', 'https://eabglobal.github.io/juniper/'),
+        ('Code', 'https://github.com/eabglobal/juniper'),
+        ('Issue tracker', 'https://github.com/eabglobal/juniper/issues'),
+    )),
+    license='Apache License 2.0',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
@@ -24,4 +35,18 @@ setup(
         'click', 'click-log', 'PyYAML'
     ],
     setup_requires=["pytest-runner"],
-    tests_require=["pytest"])
+    tests_require=["pytest"],
+    classifiers=[
+        'Development Status :: 1 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Juniper',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License 2.0',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Building Tools',
+        'Topic :: Software Development :: Libraries :: AWS Lambda Packaging',
+    ])
