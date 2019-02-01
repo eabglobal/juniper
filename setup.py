92a2a1f539f9ffe91f89694a0feeb21f86da831a
Pedro Diaz <pedro.diaz.vargas@gmail.com>
2019-02-01 18:15:04 -0500

    Minor changes

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
 
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name="juniper",
-    version='0.1',
+    version='0.1.0',
     description='',
     author='EAB Tech',
     author_email='eabtech@eab.com',
+    description="Tool to streamline the build of python lambda functions."
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
     entry_points={
