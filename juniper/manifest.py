3c6a028719e21692040175384375be6dc0ad3699
Robert Hughes <robert.hughes@bt.com>
2019-03-02 10:23:22 +0000

    Adds docstrings

--- juniper/manifest.py
+++ juniper/manifest.py
@@ -17,10 +17,22 @@ def validate_manifest_definition(manifest_definition):
 
 
 def missing(key, manifest_definition):
+    """
+    Return all file paths from the key of the manifest that don't exist.
+    :param key: A key in the manifest_definitions functions dictionary
+    :param manifest_definition: Dictionary of the manifest
+    :return: Dictionary of all paths or files that don't exist
+    """
     return [f for f in all_keys(manifest_definition, key)
             if not Path(f).exists()]
 
 
 def all_keys(manifest_definition, key):
+    """
+    Return all file paths from the key of the manifest.
+    :param manifest_definition: Dictionary of the manifest
+    :param key: A key in the manifest_definitions functions dictionary
+    :return: All paths specified for that key in the manifest file
+    """
     result = [function[key] for function in manifest_definition['functions'].values()]
     return result if isinstance(result[0], str) else chain.from_iterable(result)
