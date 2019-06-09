bd1f93c6ac33d6dcd4966219f5d790a67c86c669
Pedro Diaz <pedro.diaz.vargas@gmail.com>
2019-06-09 14:17:57 -0400

    Fixing flake8 issues

--- examples/manywheels/lambda_function.py
+++ examples/manywheels/lambda_function.py
@@ -5,6 +5,6 @@ from lxml import etree
 # https://stackoverflow.com/questions/55508626/lambda-not-loading-cryptography-shared-library
 # https://gist.github.com/vladgolubev/439559fc7597a4fb51eaa9e97b72f319
 def lambda_handler(event, context):
-    manager = xmlsec.KeysManager()
-
-    return {'hello': 'world'}
+    ctx = xmlsec.EncryptionContext(manager=xmlsec.KeysManager())
+    root = etree.Element("root")
+    return {'has_key': bool(ctx.key), 'xmlroot': root.tag}
